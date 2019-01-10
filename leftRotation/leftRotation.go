f4447f3429b6ba6be04d788ba9740d9700f36828
David Lanouette <dlanouet@cisco.com>
2019-01-10 09:02:10 -0500

    LeftRotate: Optomize the solution.

--- leftRotation/leftRotation.go
+++ leftRotation/leftRotation.go
@@ -4,26 +4,53 @@ import "fmt"
 
 func main() {
 
-	n := 5
-	d := 5
+	n := int32(5)
+	d := int32(4)
 
-	a := []int{1, 2, 3, 4, 5}
+	a := []int32{1, 2, 3, 4, 5}
+	b := a
 
-	fmt.Println("starting ", a)
+	fmt.Print("starting ")
+	printSlice(a)
 
-	for i := 0; i < d; i++ {
-		a = rotate(a, n)
-		fmt.Println(" - ", a)
+	for i := int32(0); i < d; i++ {
+		b = rotate(b, n)
+		// fmt.Println(" - ", b)
 	}
+	printSlice(b)
 
-	fmt.Println("final array: ", a)
+	c := rotate2(a, n, d)
+	printSlice(c)
 }
 
-func rotate(a []int, n int) []int {
-	result := make([]int, n, n)
+func printSlice(a []int32) {
+	for i, n := range a {
+		fmt.Print(n)
+		if i != len(a) {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Println()
+}
+
+func rotate2(a []int32, n, d int32) []int32 {
+	result := make([]int32, n, n)
+
+	for i := int32(0); i < n; i++ {
+		// fmt.Println(i, i+n, i+n+d, i+d, i+d/n, (i+d)/n, (i+d+n)%n)
+		newIndex := (i + d + n) % n
+		// fmt.Println("- ", i, newIndex, a[newIndex])
+		result[i] = a[newIndex]
+		// fmt.Println("-- ", result)
+	}
+	return result
+}
+
+func rotate(a []int32, n int32) []int32 {
+	result := make([]int32, n, n)
 
 	result[n-1] = a[0]
-	for i := 0; i < n-1; i++ {
+	for i := int32(0); i < n-1; i++ {
 		// fmt.Println(i, a[i], a[i+1])
 		result[i] = a[i+1]
 		// fmt.Println(" -- ", result)
